b920928bc07f37f2d436e2a10b3ec9114d10e43d
Ivan Nikolić <niksy5@gmail.com>
2014-11-15 21:01:50 +0100

    Switch to `indexof`

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 var ap      = Array.prototype;
 var concat  = ap.concat;
 var slice   = ap.slice;
-var indexOf = ap.indexOf;
+var indexOf = require('indexof');
 
 function except(object) {
   var result = {};
   var keys = concat.apply(ap, slice.call(arguments, 1));
 
   for (var key in object) {
-    if (indexOf.call(keys, key) === -1) {
+    if (indexOf(keys, key) === -1) {
       result[key] = object[key];
     }
   }
